ab79c553bf38c17362658a8ad55c3de0c107d281
PrageshShrestha <binormus006@gmail.com>
2026-04-11 18:18:39 +0545

    fix:reused yolo26x.pt

--- train2.py
+++ train2.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the pose estimation model
-model = YOLO("best.pt") 
+model = YOLO("yolo26x.pt") 
 
 # Train the model
-results = model.train(data="data2.yaml", epochs=100, imgsz=640)
+results = model.train(data="dataset.yaml", epochs=100, imgsz=640,batch=6)
